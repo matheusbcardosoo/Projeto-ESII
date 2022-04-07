862731c9581cb0f84039317e0795656a8d9e5f3c
ViniciusSugawara <102376635+ViniciusSugawara@users.noreply.github.com>
2022-04-07 16:09:30 -0300

    Adicionado arquivos de classe
    classe quadra cuida de tudo relacionado a quadra, com atributos e construtor (metodos como mostrarInfos, defineInfos, etc.)
    classe SoftwareEngineering eh a main, com uns testes que eu fiz pra ver se ta funcionando

---
+++ Quadra.java
@@ -0,0 +1,50 @@
+package softwareengineering;
+import java.util.Scanner;
+
+public class Quadra {
+    int numQuadra;
+    String tipoQuadra;
+    boolean coberta;
+    boolean arquibcd;
+    boolean bancoJgd;
+    Scanner scan = new Scanner(System.in);
+
+    public Quadra(int numQuadra, String tipoQuadra, boolean coberta, boolean arquibcd, boolean bancoJgd){
+        this.numQuadra = numQuadra;
+        this.tipoQuadra = tipoQuadra;
+        this.coberta = coberta;
+        this.arquibcd = arquibcd;
+        this.bancoJgd = bancoJgd;
+    }
+    
+    void defineInfo(){
+        System.out.println("Insira numero da quadra: ");
+        this.numQuadra = Integer.parseInt(scan.nextLine());
+        System.out.println("Insira tipo da quadra ");
+        this.tipoQuadra = scan.nextLine();
+        System.out.println("A quadra tem cobertura? ");
+        this.coberta = scan.nextBoolean();
+        System.out.println("A quadra tem arquibancada? ");
+        this.arquibcd = scan.nextBoolean();
+        System.out.println("A quadra tem banco para jogadores? ");
+        this.bancoJgd = scan.nextBoolean();
+    }
+    
+    String mostraInfos(){
+        return(" Numero da quadra " + this.numQuadra + ", Tipo da quadra " + this.tipoQuadra + 
+                ehCoberta(this.coberta) + temArquibcd(this.arquibcd) + temBanco(this.bancoJgd));
+    }
+    
+    String ehCoberta(boolean condicao){
+        if(condicao == true){ return(", Eh coberta"); }
+        else { return (", Nao eh coberta"); }
+    }
+    String temArquibcd(boolean condicao){
+        if(condicao == true){ return(", Tem arquibancada"); }
+        else { return(", Nao tem arquibancada"); }
+    }
+    String temBanco(boolean condicao){
+        if(this.bancoJgd == true) { return(", Tem banco para jogadores"); }
+        else { return(", Nao tem banco para jogadores"); }
+    }
+}
---
+++ SoftwareEngineering.java
@@ -0,0 +1,15 @@
+package softwareengineering;
+public class SoftwareEngineering {
+
+    //Programa para registro de quadra, projeto Engenharia de Software II
+    public static void main(String[] args) {
+        Quadra q1 = new Quadra(15, "Beachtenis", true, true, false);
+        Quadra q2 = new Quadra(22, "Tenis rapido", false, false, false);
+        Quadra q3 = new Quadra(0, "", false, false, false);
+        System.out.println(q1.mostraInfos());
+        System.out.println(q2.mostraInfos());
+        
+        q3.defineInfo();
+        System.out.println(q3.mostraInfos());
+    }
+}
