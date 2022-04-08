4e9d500a3bf9571fba86fe11f9a108ba8dd8a8ae
Matheus Barbosa <96997944+matheusbcardosoo@users.noreply.github.com>
2022-04-08 12:35:16 -0300

    add classe Cliente
    Por enquanto, coleta as informações principais do cliente (nome e cpf) e verifica se ele já realizou uma reserva anteriormente e não compareceu.

---
+++ Cliente.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class Cliente {
+    static String nomeCompleto, cpf;
+    static int naoCompareceu;
+    public static void addInfosCliente (){        
+        nomeCompleto = JOptionPane.showInputDialog ("Nome completo do cliente:");
+        cpf = JOptionPane.showInputDialog ("CPF do cliente:");
+    }
+    static void situacaoComparecer(){
+        //aux = JOptionPane.showInputDialog ("O cliente já reservou anteriormente e não compareceu?\n ATENÇÃO: \n Use 1 para sim e 2 para não!");
+        naoCompareceu = Integer.parseInt(JOptionPane.showInputDialog ("O cliente já reservou anteriormente e não compareceu?\n ATENÇÃO: \n Use 1 para sim e 2 para não!"));
+        if (naoCompareceu == 1){
+            JOptionPane.showMessageDialog(null, "ATENÇÃO!!!\nO cliente deve realizar o pagamento do valor total da reserva via PIX para confirmar a reserva!");
+        }
+    }
+}
