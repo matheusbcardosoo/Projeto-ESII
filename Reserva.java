d03cba7c9a453cad2eefe7cc8431db852116c86a
Matheus Barbosa <96997944+matheusbcardosoo@users.noreply.github.com>
2022-04-08 12:39:36 -0300

    add classe Reserva
    Possui uma clase menuReserva, onde mostra as possibilidades de acessos referente a reservas, por enquanto possui validada apenas a opção 1 - adicionar reserva, que chama o método addReserva, que por sua vez chama os métodos Cliente.addInfosCliente, Agenda.addDataHorario() e Cliente.situacaoComparecer. Além de perguntar qual será a quadra a ser alugada.

---
+++ Reserva.java
@@ -0,0 +1,30 @@
+/**
+ * Código responsavel pelo realização de agendamentos de reservas.
+ */
+import javax.swing.JOptionPane;
+
+public class Reserva {
+    static int numQuadra, escolha;
+    public static void menuReservas (String[] args) {
+        JOptionPane.showMessageDialog(null, "Acessando aba de Reservas!");
+        //aux = JOptionPane.showInputDialog("Escolha uma opção do menu para continuar \n (1) Adicionar Reserva \n (2) Consultar reservas \n (3) Cancelar reservas");
+        escolha = Integer.parseInt(JOptionPane.showInputDialog("Escolha uma opção do menu para continuar \n (1) Adicionar Reserva \n (2) Consultar reservas \n (3) Cancelar reservas"));
+        switch (escolha) {
+            case 1: addReserva(); break;
+            case 2: break;
+            case 3: break;
+            default:
+                JOptionPane.showMessageDialog(null, "Opção inválida. Tente novamente!");
+                menuReservas(args);
+                break;
+        }
+        
+    }
+    static void addReserva(){
+        Cliente.addInfosCliente();
+        //aux = JOptionPane.showInputDialog("Entre com o numero da quadra solicitada:");
+        numQuadra = Integer.parseInt(JOptionPane.showInputDialog("Entre com o numero da quadra solicitada:"));        
+        Agenda.addDataHorario();
+        Cliente.situacaoComparecer();
+    }
+}
